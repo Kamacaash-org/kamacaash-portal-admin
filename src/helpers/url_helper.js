401a3856ea3b5088389ecb2622651183a25e9396
cmmyare <cabdullaahim@st.snu.edu.so>
2026-02-09 23:04:34 +0300

    busines profile and change pass

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -1,6 +1,7 @@
 export const POST_LOGIN = "/auth/login";
 export const CHANGE_PASSWORD = "/staff/change-password";
 export const STAFF_PROFILE = "/staff/profile";
+export const BUSINESS_PROFILE = "/businesses/profile";
 
 // ==================================  URL ===================================================
 //
